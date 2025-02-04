c108708fc518526fc0cabadc91a3a9190dde3976
Adrian Ortiz <desarrollador3@en-trega.com>
2025-02-04 11:41:20 -0600

    Update

--- app.py
+++ app.py
@@ -1,34 +1,33 @@
-from flask import Flask, request, Response, jsonify
+from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
 import pyodbc
 from datetime import datetime
-from dotenv import load_dotenv
 from decimal import Decimal
-import json
+from dotenv import load_dotenv
 import os
+import json
 
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-# Credenciales de usuario
-SERVER = os.getenv('DB_SERVER')
-USER = os.getenv('DB_USER')
-PASSWORD = os.getenv('DB_PASSWORD')
-
 # Nombre de las bases de datos
 PBA_DATABASE = os.getenv('DB_PBA')
 PROD_DATABASE = os.getenv('DB_PROD')
+SERVER = os.getenv('DB_SERVER')
 
-def get_db_connection(database):
+# Lista de usuarios no permitidos
+USUARIOS_NO_PERMITIDOS = ["Entrega"]
+
+def get_db_connection(database, user, password):
     try:
         conn = pyodbc.connect(
             'DRIVER={ODBC Driver 17 for SQL Server};'
             f'SERVER={SERVER};'
             f'DATABASE={database};'
-            f'UID={USER};'
-            f'PWD={PASSWORD}'
+            f'UID={user};'
+            f'PWD={password};'
         )
         return conn
     except pyodbc.Error as e:
@@ -39,10 +38,18 @@ def get_db_connection(database):
 def sqlQuery():
     data = request.get_json()
     query = data.get('query')
-    use_prod_db = data.get('use_prod_db', False)
+    user = data.get('user')
+    password = data.get('password')
+    use_prod_db = True
+
+    if not user or not password:
+        return jsonify({"error": "Credenciales no proporcionadas correctamente."}), 400
+
+    if user in USUARIOS_NO_PERMITIDOS:
+        return jsonify({"error": f"El usuario '{user}' no está permitido."}), 403
 
     database = PROD_DATABASE if use_prod_db else PBA_DATABASE
-    conn = get_db_connection(database)
+    conn = get_db_connection(database, user, password)
     if not conn:
         return jsonify({"error": "Error al conectar a la base de datos"}), 500
 
@@ -68,7 +75,6 @@ def sqlQuery():
         cursor.close()
         conn.close()
 
-    # Convert result to JSON string with ordered keys
     result_json = json.dumps(result, ensure_ascii=False)
     return Response(result_json, mimetype='application/json')
 
@@ -79,6 +85,14 @@ def sqlLog():
     recent = data.get('recent', True)
     use_prod_db = data.get('use_prod_db', False)
     file = data.get('file', None)
+    user = data.get('user')
+    password = data.get('password')
+
+    if not user or not password:
+        return jsonify({"error": "Credenciales no proporcionadas correctamente."}), 400
+
+    if user in USUARIOS_NO_PERMITIDOS:
+        return jsonify({"error": f"El usuario '{user}' no está permitido."}), 403
 
     order_by = 'DESC' if recent else 'ASC'
     xamppPath = "C:\\xampp\\htdocs\\en-trega\\"
@@ -89,7 +103,7 @@ def sqlLog():
         query = f"SELECT TOP {num_logs} * FROM _log_error_sql ORDER BY id {order_by}"
 
     database = PBA_DATABASE if use_prod_db else PROD_DATABASE
-    conn = get_db_connection(database)
+    conn = get_db_connection(database, user, password)
 
     if not conn:
         return jsonify({"error": "Error al conectar a la base de datos"}), 500
@@ -122,12 +136,20 @@ def get_table_help():
     table = data.get('table')
     column = data.get('column', None)
     use_prod_db = data.get('use_prod_db', False)
+    user = data.get('user')
+    password = data.get('password')
+
+    if not user or not password:
+        return jsonify({"error": "Credenciales no proporcionadas correctamente."}), 400
+
+    if user in USUARIOS_NO_PERMITIDOS:
+        return jsonify({"error": f"El usuario '{user}' no está permitido."}), 403
 
     if not table:
         return jsonify({"error": "Falta el parámetro 'table'"}), 400
 
     database = PBA_DATABASE if use_prod_db else PROD_DATABASE
-    conn = get_db_connection(database)
+    conn = get_db_connection(database, user, password)
     if not conn:
         return jsonify({"error": "Error al conectar a la base de datos"}), 500
 
@@ -166,6 +188,81 @@ def get_table_help():
 
     result = json.dumps(fields)
     return Response(result, mimetype='application/json')
+
+@app.route('/api/infoTicket', methods=['POST'])
+def get_info_ticket():
+    data = request.get_json()
+    ticket = data.get('ticket')
+    use_prod_db = data.get('use_prod_db', False)
+    user = data.get('user')
+    password = data.get('password')
+    base_link = "https://erp.en-trega.com/en-trega/sis_ticket_sistema.php?accion=editar&id="
+    ticket_link = f"{base_link}{ticket}"
+
+    if not user or not password:
+        return jsonify({"error": "Credenciales no proporcionadas correctamente."}), 400
+
+    if user in USUARIOS_NO_PERMITIDOS:
+        return jsonify({"error": f"El usuario '{user}' no está permitido."}), 403
+
+    if not ticket:
+        return jsonify({"error": "Falta el parámetro 'ticket'"}), 400
+
+    database = PBA_DATABASE if use_prod_db else PROD_DATABASE
+    conn = get_db_connection(database, user, password)
+    if not conn:
+        return jsonify({"error": "Error al conectar a la base de datos"}), 500
+
+    try:
+        with conn.cursor() as cursor:
+            query = f"""
+                SELECT
+                    ts.id,
+                    CASE
+                        WHEN DATALENGTH(ts.problema_actual) > 0 THEN ts.problema_actual
+                        WHEN DATALENGTH(ts.comentario) > 0 THEN ts.comentario
+                        ELSE ''
+                    END AS problematica,
+                    u.nombre,
+                    ts.tipo,
+                    ts.fecha_proceso,
+                    CASE
+                        WHEN DATEDIFF(MINUTE, ts.fecha_proceso, GETDATE()) < 60 THEN CAST(DATEDIFF(MINUTE, ts.fecha_proceso, GETDATE()) AS VARCHAR) + ' minutos'
+                        WHEN DATEDIFF(HOUR, ts.fecha_proceso, GETDATE()) < 24 THEN 
+                            CAST(DATEDIFF(HOUR, ts.fecha_proceso, GETDATE()) AS VARCHAR) + ' horas ' +
+                            CAST(DATEDIFF(MINUTE, ts.fecha_proceso, GETDATE()) % 60 AS VARCHAR) + ' minutos'
+                        ELSE CAST(DATEDIFF(DAY, ts.fecha_proceso, GETDATE()) AS VARCHAR) + ' días'
+                    END AS tiempo_transcurrido
+                FROM
+                    _ticket_sistema ts
+                JOIN _usuarios u
+                    ON ts.login = u.login
+                WHERE
+                    ts.id = {ticket}
+                    and ts.status IN ('En Proceso', 'En Pruebas')
+            """
+
+            cursor.execute(query)
+            columns = [column[0] for column in cursor.description]
+            result = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+            for row in result:
+                for key, value in row.items():
+                    if isinstance(value, str):
+                        row[key] = value.strip()
+                    elif isinstance(value, datetime):
+                        row[key] = value.isoformat()
+                row["ticket_link"] = ticket_link
+
+    except pyodbc.Error as e:
+        print("Error al ejecutar la consulta:", e)
+        return jsonify({"error": str(e)}), 500
+    finally:
+        conn.close()
+
+    result = json.dumps(result)
+    return Response(result, mimetype='application/json')
+
 
 @app.route('/api/status', methods=['GET'])
 def get_status():
